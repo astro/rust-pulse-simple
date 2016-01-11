ca770752eaca9f712634fccc48b7fa7c0d89fc5d
Astro <astro@spaceboyz.net>
2016-01-11 02:42:30 +0100

    different approach by parameterizing over just the sample type

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,36 @@
 extern crate libpulse_sys;
+extern crate libc;
 
 use libpulse_sys::*;
 use std::ptr::{null, null_mut};
-use std::{mem, slice};
+use std::mem::{transmute, size_of};
+use std::marker::PhantomData;
 
 
-struct SimpleClient<Fmt, Channels> {
-    simple: *const pa_simple
+pub trait Sampleable {
+    fn format() -> pa_sample_format_t;
 }
 
-impl<Fmt, Channels> SimpleClient<Fmt, Channels> {
-    fn channel_count() -> u8 {
-        /* Type vars seem only usable in signatures */
-        [0; Channels].len() as u8
+impl Sampleable for i16 {
+    fn format() -> pa_sample_format_t {
+        // TODO: endianess
+        PA_SAMPLE_S16LE
     }
-    
-    fn new(name: &str, desc: &str, dir: pa_stream_direction_t, rate: u32) {
+}
+
+struct SimpleClient<S: Sampleable> {
+    simple: *mut pa_simple,
+    phantom: PhantomData<S>
+}
+
+impl<S: Sampleable> SimpleClient<S> {
+    fn new(name: &str, desc: &str, dir: pa_stream_direction_t, channel_count: u8, rate: u32) -> Self {
         let ss = pa_sample_spec {
-            format: PA_SAMPLE_S16LE,
-            channels: Self::channel_count(),
+            format: S::format(),
+            channels: channel_count,
             rate: rate
         };
+        // TODO: must create CStrings instead of as_ptr
         let s = unsafe {
             pa_simple_new(null(),             // Use the default server.
                           name.as_ptr() as *const i8,  // Our application's name.
@@ -33,23 +43,33 @@ impl<Fmt, Channels> SimpleClient<Fmt, Channels> {
                           null_mut(),         // Ignore error code.
                          )
         };
+        assert!(s != null_mut());
+        SimpleClient {
+            simple: s,
+            phantom: PhantomData
+        }
     }
     
     // TODO: Drop
 }
 
-pub struct Playback<Fmt, Channels> {
-    client: SimpleClient<Fmt, Channels>
+pub struct Playback<S: Sampleable> {
+    client: SimpleClient<S>
 }
 
-pub impl<Fmt, Channels> SimpleClient<Fmt, Channels> {
-    pub fn new(name: &str, desc: &str, rate: u32) {
+impl<S: Sampleable> Playback<S> {
+    pub fn new(name: &str, desc: &str, channel_count: u8, rate: u32) -> Self {
         Playback {
-            client: SimpleClient::new(name, desc, PA_STREAM_PLAYBACK, rate)
+            client: SimpleClient::new(name, desc, PA_STREAM_PLAYBACK, channel_count, rate)
         }
     }
     
-    pub fn write(data: &[[Fmt; Channels]]) -> std::io::Result<()> {
+    pub fn write(&self, data: &[S]) {
+        let res = unsafe {
+            let ptr = transmute(data.as_ptr());
+            pa_simple_write(self.client.simple, ptr, data.len() * size_of::<S>(), null_mut())
+        };
+        assert!(res == 0);
     }
 }
 
