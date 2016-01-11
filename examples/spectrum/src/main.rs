dfd8dcbfb75e02bbbe2f813a1b1f8ae849465d8e
Astro <astro@spaceboyz.net>
2016-01-11 17:37:37 +0100

    spectrum example with dft

--- examples/spectrum/src/main.rs
+++ examples/spectrum/src/main.rs
@@ -1,35 +1,33 @@
 extern crate pulse_simple;
-extern crate rustfft;
-extern crate num;
+extern crate dft;
 
 use pulse_simple::Record;
-use rustfft::FFT;
-use num::complex::Complex;
+use dft::{Operation, Plan};
 
 
-fn analyze_channel(fft: &mut FFT<f32>, data: &[[f32; 2]], channel: usize) -> Vec<f32> {
+fn analyze_channel(plan: &Plan, data: &[[f32; 2]], channel: usize) -> Vec<f32> {
     let mut input = Vec::with_capacity(data.len());
     for x in data {
-        input.push(Complex::new(x[channel], 0.0));
+        input.push(x[channel] as f64);
     }
     
-    let mut output = input.clone();
-    fft.process(&input[..], &mut output);
+    dft::transform(&mut input, &plan);
+    let output = dft::unpack(&input);
     
     let mut result = Vec::with_capacity(data.len());
-    for ref c in output[(output.len() / 2)..output.len()].iter() {
-        result.push(c.norm());
+    for ref c in output {
+        result.push(c.norm() as f32);
     }
     result
 }
 
 const RATE: u32 = 48000;
-const WINDOW: usize = 4800;
-const FREQS_PER_COLUMN: usize = 40;
+const WINDOW: usize = 2048;
+const FREQS_PER_COLUMN: usize = 20;
 
 fn main() {
     let p = Record::new("Example", "Record", RATE);
-    let mut fft = FFT::new(WINDOW, false);
+    let mut plan = Plan::new(Operation::Forward, WINDOW);
 
     // Fill:
     let mut data = Vec::with_capacity(WINDOW);
@@ -41,29 +39,31 @@ fn main() {
     let mut max: f32 = 0.0;
     loop {
         p.read(&mut data[..]);
-        let freqs = analyze_channel(&mut fft, &data[..], 0);
+        let freqs = analyze_channel(&mut plan, &data[..], 1);
 
         let mut top_freq = 0.0;
         let mut top_freq_volume = 0.0;
         for (i, volume) in freqs.iter().enumerate() {
-            if volume >= &top_freq_volume {
+            if i > 0 && i < freqs.len() / 2 && volume >= &top_freq_volume {
                 top_freq = i as f32 * RATE as f32 / freqs.len() as f32;
                 top_freq_volume = *volume;
             }
         }
-        println!("top: {} at {}", top_freq, top_freq_volume);
+        println!("top: {} Hz at volume {}", top_freq, top_freq_volume);
         
         let mut spectrum = Vec::with_capacity(WINDOW / FREQS_PER_COLUMN);
         max *= 0.95;  // Dampen
-        for column in 0..(WINDOW / 2 / FREQS_PER_COLUMN) {
+        for column in 0..(WINDOW / FREQS_PER_COLUMN) {
             let c1 = column * FREQS_PER_COLUMN;
             let c2 = (column + 1) * FREQS_PER_COLUMN;
-            let mut sum = 0.0;
+            let mut sum: f32 = 0.0;
             for x in c1..c2 {
                 sum += freqs[x];
             }
-            spectrum.push(sum);
-            max = max.max(sum);
+            if column > 0 && column < WINDOW / FREQS_PER_COLUMN / 2 {
+                spectrum.push(sum);
+                max = max.max(sum);
+            }
         }
 
         print!("[");
