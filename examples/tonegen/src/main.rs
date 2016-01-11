1f971f84ea86442c12282104746a8514b8662313
Astro <astro@spaceboyz.net>
2016-01-11 15:08:53 +0100

    parameterize over ChannelCount

--- examples/tonegen/src/main.rs
+++ examples/tonegen/src/main.rs
@@ -7,15 +7,14 @@ use std::f64::consts::PI;
 const RATE: u32 = 48000;
 
 fn main() {
-    let p = Playback::new("Example", "Playback", 2, RATE);
+    let p = Playback::new("Example", "Playback", RATE);
 
     // Generate 1s of sound
     let mut data = Vec::with_capacity(RATE as usize);
     for i in 0..RATE {
         let t = i as f64 / RATE as f64;
-        let mut push_freq = |f: f64| data.push(((std::i16::MAX as f64) * (f * 2.0 * PI * t).sin()) as i16);
-        push_freq(440.0);
-        push_freq(330.0);
+        let make_freq = |f: f64| ((std::i16::MAX as f64) * (f * 2.0 * PI * t).sin()) as i16;
+        data.push([make_freq(440.0), make_freq(330.0)]);
     }
 
     // Play in a loop
--- src/lib.rs
+++ src/lib.rs
@@ -48,16 +48,42 @@ impl Sampleable for i32 {
     }
 }
 
-struct SimpleClient<S: Sampleable> {
-    simple: *mut pa_simple,
-    phantom: PhantomData<S>
+pub trait ChannelCount {
+    fn count() -> u8;
+
+    type S: Sampleable;
+    fn format() -> pa_sample_format_t {
+        Self::S::format()
+    }
+    fn sample_size() -> usize {
+        Self::count() as usize * size_of::<Self::S>()
+    }
 }
 
-impl<S: Sampleable> SimpleClient<S> {
-    fn new(name: &str, desc: &str, dir: pa_stream_direction_t, channel_count: u8, rate: u32) -> Self {
+impl<S> ChannelCount for [S; 1] where S: Sampleable {
+    type S = S;
+    fn count() -> u8 {
+        1
+    }
+}
+
+impl<S> ChannelCount for [S; 2] where S: Sampleable {
+    type S = S;
+    fn count() -> u8 {
+        2
+    }
+}
+
+struct SimpleClient<C: ChannelCount> {
+    simple: *mut pa_simple,
+    phantom: PhantomData<C>,
+}
+
+impl<C: ChannelCount> SimpleClient<C> {
+    fn new(name: &str, desc: &str, dir: pa_stream_direction_t, rate: u32) -> Self {
         let ss = pa_sample_spec {
-            format: S::format(),
-            channels: channel_count,
+            format: C::format(),
+            channels: C::count(),
             rate: rate
         };
         let name_c = CString::new(name).unwrap();
@@ -82,28 +108,28 @@ impl<S: Sampleable> SimpleClient<S> {
     }
 }
 
-impl<S: Sampleable> Drop for SimpleClient<S> {
+impl<C: ChannelCount> Drop for SimpleClient<C> {
     fn drop(&mut self) {
         unsafe { pa_simple_free(self.simple) };
     }
 }
 
 
-pub struct Playback<S: Sampleable> {
-    client: SimpleClient<S>
+pub struct Playback<C: ChannelCount> {
+    client: SimpleClient<C>
 }
 
-impl<S: Sampleable> Playback<S> {
-    pub fn new(name: &str, desc: &str, channel_count: u8, rate: u32) -> Self {
+impl<C: ChannelCount> Playback<C> {
+    pub fn new(name: &str, desc: &str, rate: u32) -> Self {
         Playback {
-            client: SimpleClient::new(name, desc, PA_STREAM_PLAYBACK, channel_count, rate)
+            client: SimpleClient::new(name, desc, PA_STREAM_PLAYBACK, rate)
         }
     }
 
-    pub fn write(&self, data: &[S]) {
+    pub fn write(&self, data: &[C]) {
         let res = unsafe {
             let ptr = transmute(data.as_ptr());
-            pa_simple_write(self.client.simple, ptr, data.len() * size_of::<S>(), null_mut())
+            pa_simple_write(self.client.simple, ptr, data.len() * C::sample_size(), null_mut())
         };
         assert!(res == 0);
     }
@@ -111,12 +137,12 @@ impl<S: Sampleable> Playback<S> {
 
 #[test]
 fn test_playback() {
-    let p = Playback::new("Test", "Playback", 1, 48000);
+    let p = Playback::new("Test", "Playback", 48000);
 
     // Generate sound
     let mut data = Vec::with_capacity(4800);
     for _ in 0..4800 {
-        data.push(0);
+        data.push([0]);
     }
 
     // Play
@@ -124,21 +150,21 @@ fn test_playback() {
 }
 
 
-pub struct Record<S: Sampleable> {
-    client: SimpleClient<S>
+pub struct Record<C: ChannelCount> {
+    client: SimpleClient<C>
 }
 
-impl<S: Sampleable> Record<S> {
-    pub fn new(name: &str, desc: &str, channel_count: u8, rate: u32) -> Self {
+impl<C: ChannelCount> Record<C> {
+    pub fn new(name: &str, desc: &str, rate: u32) -> Self {
         Record {
-            client: SimpleClient::new(name, desc, PA_STREAM_RECORD, channel_count, rate)
+            client: SimpleClient::new(name, desc, PA_STREAM_RECORD, rate)
         }
     }
 
-    pub fn read(&self, data: &mut [S]) {
+    pub fn read(&self, data: &mut [C]) {
         let res = unsafe {
             let ptr = transmute(data.as_mut_ptr());
-            pa_simple_read(self.client.simple, ptr, data.len() * size_of::<S>(), null_mut())
+            pa_simple_read(self.client.simple, ptr, data.len() * C::sample_size(), null_mut())
         };
         assert!(res >= 0);
     }
@@ -146,14 +172,14 @@ impl<S: Sampleable> Record<S> {
 
 #[test]
 fn test_record() {
-    let p = Record::new("Test", "Record", 1, 48000);
+    let p = Record::new("Test", "Record", 48000);
 
     // Fill:
     let mut data = Vec::with_capacity(4800);
     for _ in 0..4800 {
-        data.push(0);
+        data.push([0, 0]);
     }
 
-    // Play
+    // Record
     p.read(&mut data[..]);
 }
